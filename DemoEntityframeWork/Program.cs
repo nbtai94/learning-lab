185fc743b3ba096ad2ad0895f883dd0425681015
NGUYEN BAO TAI <btkt.1412@gmail.com>
2026-04-07 12:50:33 +0700

    Squashed 'learn/entity/' content from commit 2fb926a
    git-subtree-dir: learn/entity
    git-subtree-split: 2fb926a0ce4b8a6e8a83599721e09b22b38a9757

---
+++ DemoEntityframeWork/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+namespace DemoEntityframeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            using (var context = new SchoolManagerDbContext())
+            {
+                var classTables = context.Set<Class>();
+                var class1 = new Class
+                {
+                    Name = "Class 12A1",
+                    Students = new Student[]
+                    {
+                     new Student
+                     {
+                         Name="Chicken"
+                     }
+                    }
+                };
+
+                classTables.Add(class1);
+                context.SaveChanges();
+
+                var students = context.Set<Student>().ToList();
+            }
+        }
+
+        public class Student
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public virtual Class Class { get; set; }
+            public int ClassId { get; set; }
+        }
+
+        public class Class
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public virtual ICollection<Student> Students { get; set; }
+        }
+    }
+
+
+}
