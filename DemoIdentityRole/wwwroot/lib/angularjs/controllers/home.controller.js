12fdc67702b363cb0cbabcc6fba86b3be66e14f4
NGUYEN BAO TAI <btkt.1412@gmail.com>
2026-04-07 12:47:57 +0700

    Squashed 'learn/angularjs/' content from commit 44f6c84
    git-subtree-dir: learn/angularjs
    git-subtree-split: 44f6c8498dbe5512b825039334adf46f609f5912

---
+++ DemoIdentityRole/wwwroot/lib/angularjs/controllers/home.controller.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    angular.module("app")
+        .controller('homeController', homeController);
+
+    homeController.$inject = ['$scope'];
+
+    function homeController($scope) {
+        $scope.title = "homcontroller ne";
+        activate();
+
+        function activate() {
+            console.log("home controller runing");
+        }
+    }
+})();
